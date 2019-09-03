be2d7243c53cb46405ea6e0a21ae2a3c8728def9
hellowuxin <hello_wuxin@qq.com>
2019-09-03 17:16:40 +0800

    修复enter事件连线无颜色的bug

--- public/js/dataJSON.js
+++ public/js/dataJSON.js
@@ -1,10 +1,11 @@
 /* global d3:true */
 /* eslint no-param-reassign: ['error', { 'props': false }] */
 let colorNumber = 0;
-const colorScale = d3.scaleOrdinal(d3.schemePaired);
+const colorScale = d3.scaleOrdinal(d3.schemePaired);// 颜色列表
 function isEqualJSON(a, b) { // 判断a，b是否完全一致
   // 局限性：
-  // 如果对象里属性的位置发生变化，转换来的字符串就不相等，但实际我们只需要看他们的内容是否一致，与顺序没有关系，所以这种方法有局限性。
+  // 如果对象里属性的位置发生变化，转换来的字符串就不相等
+  // 但实际我们只需要看他们的内容是否一致，与顺序没有关系，所以这种方法有局限性。
   const aStr = JSON.stringify(a);
   const bStr = JSON.stringify(b);
   if (aStr === bStr) {
@@ -29,7 +30,7 @@ function breadthTraverse(d, c) { // 广度遍历
     }
   }
 }
-function inheritColor(d, c) {
+function inheritColor(d, c) { // 赋予新颜色，并更新子节点的颜色
   if (d.children) {
     for (let index = 0; index < d.children.length; index += 1) {
       const dChild = d.children[index];
@@ -122,6 +123,13 @@ class DataJSON { // eslint-disable-line
     for (let index = 0; index < data.length; index += 1) {
       const dataChild = data[index];
       if (isEqualJSON(dataChild, dPosition)) {
+        if (dPosition.id.substring(0, 1) === '0' && dPosition.id.length === 2) { // 根节点的直接子节点
+          colorNumber += 1;
+          d.color = colorScale(colorNumber);
+        } else {
+          d.color = dPosition.color;
+        }
+        inheritColor(d, d.color);
         data.splice(index + i, 0, d);
         return true;
       }
